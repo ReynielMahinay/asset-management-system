04302a564870fcd01d35b1ed9485b0ac4da5aa0f
Reyniel Mahinay <reyniel.t.mahinay@gmail.com>
2025-08-14 09:23:34 +0800

    Adding table user

--- src/features/manage_user/ManageUser.jsx
+++ src/features/manage_user/ManageUser.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import UserTable from "../../components/common/UserTable";
 
 function ManageUser() {
-  return <div className="bg-white">ManageUser</div>;
+  return (
+    <div className="bg-white">
+      <UserTable />
+    </div>
+  );
 }
 
 export default ManageUser;
