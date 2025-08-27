f3a96ea17f6a9c5d4d87d6b9e4fd09489a734496
Reyniel Mahinay <reyniel.t.mahinay@gmail.com>
2025-08-27 10:51:30 +0800

    Adding the backend file structure and running a sample database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,2 +1,20 @@
 const express = require('express')
+const cors = require('cors')
 const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Backend is running',
+        status: 'OK',
+        timestamp: new Date().toISOString()
+    })
+})
+
+module.exports = app
+
