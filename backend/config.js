0a38831c3ad7d37e01b089625bcdac911aed22df
Reyniel Mahinay <reyniel.t.mahinay@gmail.com>
2026-06-06 11:13:49 +0800

    fix: use FRONTEND_URL instead of VITE_VERCEL_URL

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const config = {
   PORT: process.env.PORT || 3000,
-  FRONTEND_URL: process.env.VITE_VERCEL_URL || "http://localhost:5173",
+  FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:5173",
   DATABASE_URL: process.env.DATABASE_URL,
   API_SECRET: process.env.API_SECRET,
 };
